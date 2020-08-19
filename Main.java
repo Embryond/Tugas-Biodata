9e10718c37fc07b98f06102f88e9cb404b0004ad
Embryond <69889336+Embryond@users.noreply.github.com>
2020-08-19 17:18:00 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nama="Krisantus Preyanda embryond";
+        String jk="Laki-laki";
+        String Alt="Suwayuwo Perumahan OIK";
+        String jurusan="RPL";
+
+        System.out.println("Biodata Saya:");
+        System.out.println("_____________");
+        System.out.println("Nama \t\t\t\t :"+nama);
+        System.out.println("Jenis Kelamin \t\t :"+jk);
+        System.out.println("Alamat \t\t\t\t :"+Alt);
+        System.out.println("Jurusan \t\t\t :"+jurusan);
+
+    }
+}
